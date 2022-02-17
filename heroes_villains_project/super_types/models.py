fe5475e4a701997fb3e2376d9807a6cfe3bb0d6f
Sydney Wilson <syd.toon17@gmail.com>
2022-02-17 14:07:58 -0600

    setup tables + added supers app + added super_types app

---
+++ heroes_villains_project/super_types/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Super_Tpyes(models.Model):
+    type = models.CharField(max_length=255)
---
+++ heroes_villains_project/supers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from super_types.models import Super_Tpyes
+
+# Create your models here.
+class Supers(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_type = models.ForeignKey(Super_Tpyes, on_delete=models.CASCADE)
