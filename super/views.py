9464d01529a112df58e0a00867aa5e4d9ded0a92
Sydney Wilson <syd.toon17@gmail.com>
2022-02-18 16:57:31 -0600

    end of day commit still working on filter heros and villains

--- super/views.py
+++ super/views.py
@@ -3,16 +3,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from  rest_framework import status
 from .serializers import SuperSerializer
+from super_types.serializers import Super_TypesSerializer
 from .models import Super
 from super_types.models import Super_Types
+
 
 
 @api_view(['GET', 'POST'])
 def super_list(request):
     if request.method == 'GET':
         super = Super.objects.all()
-        super_type = Super_Types.objects.all()
-        serializer = SuperSerializer(super, many=True)
+        hero = Super_Types.objects.filter(pk=2)
+        villain = Super_Types.objects.filter(pk=2)
+        serializer = SuperSerializer(super, hero, villain, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
